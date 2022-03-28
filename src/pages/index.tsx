1695bb3b6fb05f160336c1999b2c202c0875a4a8
manak1 <mana23happy@hotmail.co.jp>
2022-03-28 20:23:26 +0900

    🌟 feat: ログイン/ログアウト/ユーザー情報取得の確認

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,26 @@ import type { NextPage } from "next"
 import InputText from "~/components/ui/InputText"
 import { useState } from "react"
 
+import { useAuth0 } from "@auth0/auth0-react"
+import UserProfile from "~/components/model/user/UserProfile"
+
 const Home: NextPage = () => {
   const [value, setValue] = useState("")
+  const { loginWithRedirect, logout } = useAuth0()
+
   return (
-    <InputText
-      value={value}
-      onChange={setValue}
-      placeholder="入力してね"
-      label="ラベル"
-      id="入力"
-    />
+    <>
+      <InputText
+        value={value}
+        onChange={setValue}
+        placeholder="入力してね"
+        label="ラベル"
+        id="入力"
+      />
+      <UserProfile />
+      <button onClick={() => loginWithRedirect()}>login</button>
+      <button onClick={() => logout()}>logout</button>
+    </>
   )
 }
 
